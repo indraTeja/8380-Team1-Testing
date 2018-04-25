1563f5c8e9c7255dcce26cde64220477dd51f146
indraTeja <indrateja@gmail.com>
2018-04-25 18:24:55 -0500

    login changes

--- 001_home_login.py
+++ 001_home_login.py
@@ -1,43 +1,37 @@
 import time
 import unittest
-from lib2to3.pgen2 import driver
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class Blog_ATS(unittest.TestCase):
+class Byzantium(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
 
-    # open https://byzantium-travel.herokuapp.com and navigate to Login
-    def test_blog(self):
+    # open https://byzantium-final.herokuapp.com and navigate to Login
+    def test_login(self):
         #test passwords
         user = "instructor"
         pwd = "instructor1a"
         driver = self.driver
         driver.maximize_window()
-        driver.get("https://byzantium-travel.herokuapp.com/home/")
-        time.sleep(1)
+        driver.get("https://byzantium-final.herokuapp.com/")
+        time.sleep(2)
 
-        elem = driver.find_element_by_xpath("/html/body/nav/nav[2]/div/div/ul[1]/li[4]/a").click()
+        elem = driver.find_element_by_xpath("/html/body/nav/nav[2]/div/div/ul[1]/li[3]/a")
+        elem.click()
         time.sleep(5)
 
         # Login Test scripts go
         elem = driver.find_element_by_id("id_username")
         elem.send_keys(user)
+        time.sleep(3)
         elem = driver.find_element_by_id("id_password")
         elem.send_keys(pwd)
         elem.send_keys(Keys.RETURN)
-
-
-
-
         time.sleep(5)
-        elem = driver.find_element_by_xpath("/html/body/nav/nav[2]/div/div/ul[1]/li[5]/a").click()
-        time.sleep(5)
-
+        #elem = driver.find_element_by_xpath("/html/body/nav/nav[2]/div/div/ul[1]/li[5]/a").click()
 
         def tearDown(self):
             self.driver.close()
